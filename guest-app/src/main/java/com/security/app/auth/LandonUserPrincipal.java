16cb155115b728b89199ed7fbce6a9f6a16d10bb
Paula Ribeiro <paula.ribeiro@dtidigital.com.br>
2020-03-23 10:30:19 -0300

    Adds UserDetails implementation

---
+++ guest-app/src/main/java/com/security/app/auth/LandonUserPrincipal.java
@@ -0,0 +1,59 @@
+package com.security.app.auth;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.security.app.auth.domain.User;
+
+public class LandonUserPrincipal implements UserDetails {
+
+	private static final long serialVersionUID = -2682968297363477273L;
+	
+	private User user;
+
+	public LandonUserPrincipal(User user) {
+		super();
+		this.user = user;
+
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
