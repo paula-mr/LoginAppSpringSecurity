7f0f6ff41a82d8ee6d070adab3d73d45df7ce794
Paula Ribeiro <paula.ribeiro@dtidigital.com.br>
2020-03-23 14:03:29 -0300

    Adds authorization to UserDetails implementation

--- guest-app/src/main/java/com/security/app/auth/LandonUserPrincipal.java
+++ guest-app/src/main/java/com/security/app/auth/LandonUserPrincipal.java
@@ -2,11 +2,15 @@ package com.security.app.auth;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.security.app.auth.domain.AuthGroup;
 import com.security.app.auth.domain.User;
 
 public class LandonUserPrincipal implements UserDetails {
@@ -14,16 +18,27 @@ public class LandonUserPrincipal implements UserDetails {
 	private static final long serialVersionUID = -2682968297363477273L;
 	
 	private User user;
+	private List<AuthGroup> authGroups;
 
-	public LandonUserPrincipal(User user) {
+	public LandonUserPrincipal(User user, List<AuthGroup> authGroups) {
 		super();
 		this.user = user;
-
+		this.authGroups = authGroups;
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+		if (authGroups == null) {
+			return Collections.emptySet();
+		}
+		
+		Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+		
+		authGroups.forEach(group -> {
+			grantedAuthorities.add(new SimpleGrantedAuthority(group.getAuthGroup()));
+		});
+		
+		return grantedAuthorities;
 	}
 
 	@Override
