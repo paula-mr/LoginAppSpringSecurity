a7f686a1f8188e45bd778c8e5fcea016c3bfc2d9
Paula Ribeiro <paula.ribeiro@dtidigital.com.br>
2020-03-23 14:27:03 -0300

    Changes from httpbasic to forms based authentication

--- guest-app/src/main/java/com/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/security/app/ApplicationSecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.security.app.auth.LandonUserDetailsService;
 
@@ -18,32 +19,32 @@ import com.security.app.auth.LandonUserDetailsService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	private final LandonUserDetailsService userDetailsService;
-	
+
 	public ApplicationSecurityConfiguration(LandonUserDetailsService userDetailsService) {
 		this.userDetailsService = userDetailsService;
 	}
-	
+
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
 		provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
 		provider.setAuthoritiesMapper(authoritiesMapper());
-		
+
 		return provider;
 	}
-	
+
 	@Bean
 	public GrantedAuthoritiesMapper authoritiesMapper() {
 		SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
 		authorityMapper.setConvertToUpperCase(true);
 		authorityMapper.setDefaultAuthority("USER");
-		
+
 		return authorityMapper;
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
@@ -52,7 +53,10 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/", "/index", "/css/*", "/js/*").permitAll().anyRequest()
-				.authenticated().and().httpBasic();
-	}	
+				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.invalidateHttpSession(true).clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/logout-success")
+				.permitAll();
+	}
 
 }
