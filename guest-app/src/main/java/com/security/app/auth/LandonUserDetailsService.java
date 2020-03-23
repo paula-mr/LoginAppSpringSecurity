146136b426f61d294e99a2910b3b97a1bd5b6b78
Paula Ribeiro <paula.ribeiro@dtidigital.com.br>
2020-03-23 14:06:44 -0300

    Adds authorization to UserDetailsService implementation

--- guest-app/src/main/java/com/security/app/auth/LandonUserDetailsService.java
+++ guest-app/src/main/java/com/security/app/auth/LandonUserDetailsService.java
@@ -1,30 +1,39 @@
 package com.security.app.auth;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.security.app.auth.domain.AuthGroup;
 import com.security.app.auth.domain.User;
+import com.security.app.auth.repository.AuthGroupRepository;
 import com.security.app.auth.repository.UserRepository;
 
 @Service
 public class LandonUserDetailsService implements UserDetailsService {
 	
-	private final UserRepository repository;
+	private final UserRepository userRepository;
+	private final AuthGroupRepository authGroupRepository;
 	
-	public LandonUserDetailsService(UserRepository repository) {
-		this.repository = repository;
+	
+	public LandonUserDetailsService(UserRepository userRepository, AuthGroupRepository authGroupRepository) {
+		this.userRepository = userRepository;
+		this.authGroupRepository = authGroupRepository;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = repository.findByUsername(username);
+		User user = userRepository.findByUsername(username);
 		
 		if (user == null) {
 			throw new UsernameNotFoundException("cannot find username: " + username);
 		}
 		
-		return new LandonUserPrincipal(user);
+		List<AuthGroup> authGroups = authGroupRepository.findByUsername(username);
+		
+		return new LandonUserPrincipal(user, authGroups);
 	}
 }
